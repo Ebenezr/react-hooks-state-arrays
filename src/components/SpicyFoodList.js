e34e792a263755c3a039e45c8a900eeb26be9731
Ebenezr <ebenezarbukosia@gmail.com>
2022-07-14 19:56:41 +0300

    Done.

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,58 +3,67 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-  const [filterBy, setFilterBy] = useState("All");
-
-
+  const [select, setSelect] = useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    const newFoodArray = [...foods, newFood];
-    setFoods(newFoodArray);
-  }
-
-  function handleLiClick(id){
-    const newFoodArray = foods.filter((food) => food.id !== id);
-    setFoods(newFoodArray);
-  }
-
-  function handleFilterChange(event) {
-    setFilterBy(event.target.value);
+    setFoods(
+      [...foods, newFood]
+    )
   }
   
-  return (
-    <select name="filter" onChange={handleFilterChange}>
-      <option value="All">All</option>
-      <option value="American">American</option>
-      <option value="Sichuan">Sichuan</option>
-      <option value="Thai">Thai</option>
-      <option value="Mexican">Mexican</option>
-    </select>
-  );
+  const handleClick = (id) => {
+    // const filtered = foods.filter( food => food.id !== id);
+    const updated = foods.map(food => {
+      if(food.id === id){
+        return {
+          ...food,
+          heatLevel:food.heatLevel += 1
+        }
+      }
+      else{
+        return food
+      }
+      
+    })
+    setFoods(updated)
+  }
 
+  const filteredFood =
+    foods.filter(food => {
+      if(select === "All"){
+        return true;
+      }
+      else{
+        return food.cuisine === select;
+      }
+    })
   
-  const foodsToDisplay = foods.filter((food) => {
-    if (filterBy === "All") {
-      return true;
-    } else {
-      return food.cuisine === filterBy;
-    }
-  });
 
-  
-
-  const foodList = foodsToDisplay.map((food) => (
-    <li key={food.id} onClick={() => handleLiClick(food.id)}>
+  // const foodList = foods.map((food) => (
+  //   <li key={food.id} onClick={()=>handleClick(food.id)}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+  const foodList = filteredFood.map((food) => (
+    <li key={food.id} onClick={()=>handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
- 
+
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={(event)=> setSelect(event.target.value)}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+</select>
     </div>
   );
 }
